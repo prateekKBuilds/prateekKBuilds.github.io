fa7fa5b68fc24aab3065f015ec23512b770a78f9
Prateek Kumar <prateek.kumar2177@gmail.com>
2026-02-05 11:16:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 // (Dark mode toggle, animations, etc.)
 
 console.log("prateekKBuilds portfolio loaded 🚀");
+
+document.getElementById("scrollTop").addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});
+
